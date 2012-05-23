f8b8763d554f17c0dac0ad821b7edb1390e357cf
Artem Kirillov <akirillov@griddynamics.com>
2012-05-23 15:39:38 +0400

    Initial commit for Jagger WebClient

---
+++ webclient/src/main/java/com/griddynamics/jagger/webclient/gwt/client/JaggerWebClient.java
@@ -0,0 +1,13 @@
+package com.griddynamics.jagger.webclient.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * @author "Artem Kirillov" (akirillov@griddynamics.com)
+ * @since 5/23/12
+ */
+public class JaggerWebClient implements EntryPoint {
+    @Override
+    public void onModuleLoad() {
+    }
+}
