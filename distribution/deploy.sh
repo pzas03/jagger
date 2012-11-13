f2febab534a99c390dc8571f17f63494e79781f0
Nikolay Musienko <nmusienko@griddynamics.com>
2012-11-13 19:08:24 +0400

    Changed hostnames and username in configurations

--- distribution/deploy.sh
+++ distribution/deploy.sh
@@ -8,30 +8,30 @@ fi
 
 chmod go-rwx ./ssh/jagger.vm.ssh.key
 
-JAGGER_HOME=/home/jagger-ci/runned_jagger
+JAGGER_HOME=/home/jagger-jenkins/runned_jagger
 PACKAGE=jagger-distribution-$RELEASE_TYPE-full.zip
 DISTRIB=jagger-distribution-$RELEASE_TYPE
 
 function do_on_vm {
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key jagger-ci@macys-jagger$1.vm.griddynamics.net $2
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key jagger-jenkins@jagger-ci$1.vm.griddynamics.net $2
 }
 
 function do_on_vm_daemon {
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key jagger-ci@macys-jagger$1.vm.griddynamics.net $2&
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key jagger-jenkins@jagger-ci$1.vm.griddynamics.net $2&
 }
 
 for i in 1 2
 do
-	echo TRYING TO DEPLOY NODE jagger-ci@macys-jagger$i.vm.griddynamics.net
+	echo TRYING TO DEPLOY NODE jagger-jenkins@jagger-ci$i.vm.griddynamics.net
 	do_on_vm $i "rm -rf $JAGGER_HOME"
 	do_on_vm $i "mkdir $JAGGER_HOME"
 
-	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key target/$PACKAGE jagger-ci@macys-jagger$i.vm.griddynamics.net:$JAGGER_HOME/$PACKAGE
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key target/$PACKAGE jagger-jenkins@jagger-ci$i.vm.griddynamics.net:$JAGGER_HOME/$PACKAGE
 	do_on_vm $i "unzip $JAGGER_HOME/$PACKAGE -d $JAGGER_HOME"
 
-    echo KILLING PREVIOUS PROCESS jagger-ci@macys-jagger$i.vm.griddynamics.net
+    echo KILLING PREVIOUS PROCESS jagger-jenkins@jagger-ci$i.vm.griddynamics.net
 	do_on_vm $i "$JAGGER_HOME/$DISTRIB/stop.sh"
-    do_on_vm $i "rm -rf /home/jagger-ci/jaggerdb"
+    do_on_vm $i "rm -rf /home/jagger-jenkins/jaggerdb"
 done
 
 echo sleep 3 sec
@@ -40,7 +40,7 @@ sleep 3
 echo Starting Kernels
 for i in 2
 do
-    echo "macys-jagger$i.vm.griddynamics.net : cd $JAGGER_HOME/jagger-$RELEASE_TYPE; ./start.sh profiles/ci-distributed/environment-kernel.properties"
+    echo "jagger-ci$i.vm.griddynamics.net : cd $JAGGER_HOME/jagger-$RELEASE_TYPE; ./start.sh profiles/ci-distributed/environment-kernel.properties"
 	do_on_vm_daemon $i "cd $JAGGER_HOME/jagger-$RELEASE_TYPE; ./start.sh profiles/ci-distributed/environment-kernel.properties"
 done
 
@@ -48,7 +48,7 @@ echo sleep 10 sec
 sleep 10
 
 echo Starting master
-echo "macys-jagger1.vm.griddynamics.net cd $JAGGER_HOME/jagger-$RELEASE_TYPE; ./start.sh profiles/ci-distributed/environment-master.properties"
+echo "jagger-ci1.vm.griddynamics.net cd $JAGGER_HOME/jagger-$RELEASE_TYPE; ./start.sh profiles/ci-distributed/environment-master.properties"
 do_on_vm 1 "cd $JAGGER_HOME/jagger-$RELEASE_TYPE; ./start.sh profiles/ci-distributed/environment-master.properties"
 
 exit
--- distribution/deploy_multiple_kernels.sh
+++ distribution/deploy_multiple_kernels.sh
@@ -8,33 +8,33 @@ fi
 
 chmod go-rwx ./ssh/jagger.vm.ssh.key
 
-JAGGER_HOME=/home/jagger-ci/runned_jagger
+JAGGER_HOME=/home/jagger-jenkins/runned_jagger
 PACKAGE=jagger-distribution-$RELEASE_TYPE-full.zip
 DISTRIB=jagger-distribution-$RELEASE_TYPE
 
 function do_on_vm {
     echo remote run : $2
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key jagger-ci@macys-jagger$1.vm.griddynamics.net $2
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key jagger-jenkins@jagger-ci$1.vm.griddynamics.net $2
 }
 
 function do_on_vm_daemon {
     echo remote run like demon : $2
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key jagger-ci@macys-jagger$1.vm.griddynamics.net $2&
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key jagger-jenkins@jagger-ci$1.vm.griddynamics.net $2&
 }
 
 for i in 1 2 3 4 5
 do
-	echo TRYING TO DEPLOY NODE jagger-ci@macys-jagger$i.vm.griddynamics.net
+	echo TRYING TO DEPLOY NODE jagger-jenkins@jagger-ci$i.vm.griddynamics.net
 	do_on_vm $i "rm -rf $JAGGER_HOME"
 	do_on_vm $i "mkdir $JAGGER_HOME"
 
-	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key target/$PACKAGE jagger-ci@macys-jagger$i.vm.griddynamics.net:$JAGGER_HOME/$PACKAGE
+	scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ./ssh/jagger.vm.ssh.key target/$PACKAGE jagger-jenkins@jagger-ci$i.vm.griddynamics.net:$JAGGER_HOME/$PACKAGE
 	do_on_vm $i "unzip $JAGGER_HOME/$PACKAGE -d $JAGGER_HOME"
 
-    echo KILLING PREVIOUS PROCESS jagger-ci@macys-jagger$i.vm.griddynamics.net
+    echo KILLING PREVIOUS PROCESS jagger-jenkins@jagger-ci$i.vm.griddynamics.net
 	do_on_vm $i "$JAGGER_HOME/$DISTRIB/stop.sh"
 	do_on_vm $i "$JAGGER_HOME/$DISTRIB/stop_agent.sh"
-    do_on_vm $i "rm -rf /home/jagger-ci/jaggerdb"
+    do_on_vm $i "rm -rf /home/jagger-jenkins/jaggerdb"
 done
 
 echo sleep 3 sec
@@ -43,22 +43,22 @@ sleep 3
 echo Starting Kernels
 for i in 2 3
 do
-    echo "macys-jagger$i.vm.griddynamics.net : cd $JAGGER_HOME/$DISTRIB; ./start.sh profiles/ci-distributed/environment-kernel.properties"
+    echo "jagger-ci$i.vm.griddynamics.net : cd $JAGGER_HOME/$DISTRIB; ./start.sh profiles/ci-distributed/environment-kernel.properties"
 	do_on_vm_daemon $i "cd $JAGGER_HOME/$DISTRIB; ./start.sh profiles/ci-distributed/environment-kernel.properties $1 $2 $3 $4 $5 $6 $7 $8"
 done
 
 echo Starting Agents
 for i in 4 5
 do
-    echo "macys-jagger$i.vm.griddynamics.net : cd $JAGGER_HOME/$DISTRIB; ./start_agent.sh"
-	do_on_vm_daemon $i "cd $JAGGER_HOME/$DISTRIB; ./start_agent.sh -Dchassis.coordination.http.url=http://macys-jagger1.vm.griddynamics.net:8089 $1 $2 $3 $4 $5 $6 $7 $8"
+    echo "jagger-ci$i.vm.griddynamics.net : cd $JAGGER_HOME/$DISTRIB; ./start_agent.sh"
+	do_on_vm_daemon $i "cd $JAGGER_HOME/$DISTRIB; ./start_agent.sh -Dchassis.coordination.http.url=http://jagger-ci1.vm.griddynamics.net:8089 $1 $2 $3 $4 $5 $6 $7 $8"
 done
 
 echo sleep 3 sec
 sleep 3
 
 echo Starting master
-echo "macys-jagger1.vm.griddynamics.net cd $JAGGER_HOME/$DISTRIB; ./start.sh profiles/ci-distributed/environment-master.properties"
+echo "jagger-ci1.vm.griddynamics.net cd $JAGGER_HOME/$DISTRIB; ./start.sh profiles/ci-distributed/environment-master.properties"
 do_on_vm 1 "cd $JAGGER_HOME/$DISTRIB; ./start.sh profiles/ci-distributed/environment-master.properties $1 $2 $3 $4 $5 $6 $7 $8"
 
 exit
