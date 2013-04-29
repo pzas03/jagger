7f6a18e2714aa38415f36c7c5334c33dc7ff2b52
Gribov Kirill <kgribov@griddynamics.com>
2013-04-29 13:05:43 +0400

    Add an ability to show custom metris in web UI

--- webclient/src/main/java/com/griddynamics/jagger/webclient/server/MetricDataServiceImpl.java
+++ webclient/src/main/java/com/griddynamics/jagger/webclient/server/MetricDataServiceImpl.java
@@ -1,5 +1,7 @@
 package com.griddynamics.jagger.webclient.server;
 
+import com.griddynamics.jagger.engine.e1.aggregator.workload.model.DiagnosticResultEntity;
+import com.griddynamics.jagger.engine.e1.aggregator.workload.model.ValidationResultEntity;
 import com.griddynamics.jagger.engine.e1.aggregator.workload.model.WorkloadProcessLatencyPercentile;
 import com.griddynamics.jagger.webclient.client.MetricDataService;
 import com.griddynamics.jagger.webclient.client.dto.MetricDto;
@@ -12,6 +14,8 @@ import org.slf4j.LoggerFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.text.ParseException;
 import java.util.*;
 
 /**
@@ -76,26 +80,16 @@ public class MetricDataServiceImpl implements MetricDataService {
 
         if (standardMetrics.containsKey(metricName.getName())){
             //it is a standard metric
-            List<Object[]> result = entityManager.createNativeQuery("SELECT " +
-                                                                    "    workload."+ standardMetrics.get(metricName.getName())+", taskData.id, taskData.sessionId " +
-                                                                    "FROM " +
-                                                                    "    WorkloadTaskData workload " +
-                                                                    "        left outer join " +
-                                                                    "    (select " +
-                                                                    "        id, taskId, sessionId " +
-                                                                    "    from " +
-                                                                    "        TaskData) as taskData ON taskData.taskId = workload.taskId " +
-                                                                    "        and taskData.sessionId = workload.sessionId " +
-                                                                    "WHERE " +
-                                                                    "    taskData.id in (:ids)").setParameter("ids", metricName.getTests().getIds()).getResultList();
+            List<Object[]> result = entityManager.createQuery("select workload."+ standardMetrics.get(metricName.getName())+", workload.sessionId "+
+                                                                    "from WorkloadTaskData as workload " +
+                                                              "where (workload.taskId, workload.sessionId) " +
+                                                                    "in (select taskData.taskId, taskData.sessionId from TaskData as taskData where taskData.id in (:ids))").setParameter("ids", metricName.getTests().getIds()).getResultList();
 
             for (Object[] temp : result){
                 String metricValue = temp[0].toString();
-                long testId =  ((BigInteger)temp[1]).longValue();
-                long sessionId = Long.parseLong(temp[2].toString());
+                long sessionId = Long.parseLong(temp[1].toString());
 
                 MetricValueDto value = new MetricValueDto();
-                value.setTestId(testId);
                 value.setValue(metricValue);
                 value.setSessionId(sessionId);
 
@@ -105,10 +99,10 @@ public class MetricDataServiceImpl implements MetricDataService {
             if (metricName.getName().matches("Latency .+ %")){
                 //it is a latency metric
                 Double latencyKey = Double.parseDouble(metricName.getName().split(" ")[1]);
-                List<Object[]> latency = entityManager.createQuery(
-                        "select s.percentileValue, s.workloadProcessDescriptiveStatistics.taskData.id, s.workloadProcessDescriptiveStatistics.taskData.sessionId from  WorkloadProcessLatencyPercentile as s " +
-                                "where s.workloadProcessDescriptiveStatistics.taskData.id in (:taskIds) " +
-                                "      and s.percentileKey=:latencyKey ")
+                List<Object[]> latency = entityManager.createQuery("select s.percentileValue, s.workloadProcessDescriptiveStatistics.taskData.id, s.workloadProcessDescriptiveStatistics.taskData.sessionId " +
+                                                                        "from  WorkloadProcessLatencyPercentile as s " +
+                                                                   "where s.workloadProcessDescriptiveStatistics.taskData.id in (:taskIds) " +
+                                                                          "and s.percentileKey=:latencyKey ")
                         .setParameter("taskIds", metricName.getTests().getIds())
                         .setParameter("latencyKey", latencyKey)
                         .getResultList();
@@ -121,7 +115,46 @@ public class MetricDataServiceImpl implements MetricDataService {
                 }
             }else{
                 //custom metric
+                List<Object[]> metrics = entityManager.createQuery("select metric, metric.workloadData.sessionId " +
+                                                                        "from DiagnosticResultEntity as metric " +
+                                                                   "where metric.name=:name " +
+                                                                            "and (metric.workloadData.taskId, metric.workloadData.sessionId) " +
+                                                                                "in (select taskData.taskId, taskData.sessionId from TaskData as taskData where taskData.id in (:ids))").setParameter("ids", metricName.getTests().getIds()).setParameter("name", metricName.getName()).getResultList();
 
+                if (!metrics.isEmpty()){
+                    for (Object[] mas : metrics){
+                        MetricValueDto value = new MetricValueDto();
+
+                        DiagnosticResultEntity metric = (DiagnosticResultEntity)mas[0];
+                        value.setValue(metric.getTotal().toString());
+
+                        value.setSessionId(Long.parseLong(mas[1].toString()));
+                        dto.getValues().add(value);
+                    }
+                }else{
+                    List<Object[]> validators = entityManager.createQuery("select metric, metric.workloadData.sessionId " +
+                                                                                "from ValidationResultEntity as metric " +
+                                                                          "where metric.validator=:name " +
+                                                                                   "and (metric.workloadData.taskId, metric.workloadData.sessionId) " +
+                                                                                        "in (select taskData.taskId, taskData.sessionId from TaskData as taskData where taskData.id in (:ids))").setParameter("ids", metricName.getTests().getIds()).setParameter("name", metricName.getName()).getResultList();
+                    for (Object[] mas : validators){
+                        MetricValueDto value = new MetricValueDto();
+
+                        ValidationResultEntity validator = (ValidationResultEntity)mas[0];
+
+                        Integer notFailed = validator.getTotal()-validator.getFailed();
+                        Double result = (double)notFailed/validator.getTotal();
+
+                        String resultString = result.toString();
+                        if (resultString.length() > 4){
+                            resultString = resultString.substring(0, 4);
+                        }
+                        value.setValue(resultString);
+
+                        value.setSessionId(Long.parseLong(mas[1].toString()));
+                        dto.getValues().add(value);
+                    }
+                }
             }
         }
 
@@ -129,31 +162,56 @@ public class MetricDataServiceImpl implements MetricDataService {
     }
 
     public Set<MetricNameDto> getCustomMetricsNames(TaskDataDto tests){
-        return Collections.EMPTY_SET;
+        Set<MetricNameDto> metrics;
+
+        List<String> metricNames = entityManager.createQuery("select metric.name from DiagnosticResultEntity as metric where " +
+                                                                "(metric.workloadData.taskId, metric.workloadData.sessionId) in " +
+                                                                    "(select taskData.taskId, taskData.sessionId from TaskData as taskData where taskData.id in (:ids))").setParameter("ids", tests.getIds()).getResultList();
+
+        List<String> validatorNames = entityManager.createQuery("select metric.validator from ValidationResultEntity as metric where " +
+                                                                    "(metric.workloadData.taskId, metric.workloadData.sessionId) in " +
+                                                                        "(select taskData.taskId, taskData.sessionId from TaskData as taskData where taskData.id in (:ids))").setParameter("ids", tests.getIds()).getResultList();
+
+        metrics = new HashSet<MetricNameDto>(metricNames.size()+validatorNames.size());
+
+        for (String name : metricNames){
+            MetricNameDto metric = new MetricNameDto();
+            metric.setTests(tests);
+            metric.setName(name);
+
+            metrics.add(metric);
+        }
+
+        for (String name : validatorNames){
+            MetricNameDto validator = new MetricNameDto();
+            validator.setTests(tests);
+            validator.setName(name);
+
+            metrics.add(validator);
+        }
+
+        return metrics;
     }
 
     public Set<MetricNameDto> getLatencyMetricsNames(TaskDataDto tests){
-        Set<MetricNameDto> latencyNames = Collections.EMPTY_SET;
-        try {
-            List<WorkloadProcessLatencyPercentile> latency = entityManager.createQuery(
-                    "select s from  WorkloadProcessLatencyPercentile as s where s.workloadProcessDescriptiveStatistics.taskData.id in (:taskIds) " +
-                            "group by s.percentileKey " +
-                            "having count(s.id)=:size")
-                    .setParameter("taskIds", tests.getIds())
-                    .setParameter("size", (long)tests.getIds().size())
-                    .getResultList();
+        Set<MetricNameDto> latencyNames;
 
-            latencyNames = new HashSet<MetricNameDto>(latency.size());
-            if (!latency.isEmpty()){
-                for(WorkloadProcessLatencyPercentile percentile : latency) {
-                    MetricNameDto dto = new MetricNameDto();
-                    dto.setName("Latency "+Double.toString(percentile.getPercentileKey())+" %");
-                    dto.setTests(tests);
-                    latencyNames.add(dto);
-                }
+        List<WorkloadProcessLatencyPercentile> latency = entityManager.createQuery(
+                "select s from  WorkloadProcessLatencyPercentile as s where s.workloadProcessDescriptiveStatistics.taskData.id in (:taskIds) " +
+                        "group by s.percentileKey " +
+                        "having count(s.id)=:size")
+                .setParameter("taskIds", tests.getIds())
+                .setParameter("size", (long) tests.getIds().size())
+                .getResultList();
+
+        latencyNames = new HashSet<MetricNameDto>(latency.size());
+        if (!latency.isEmpty()){
+            for(WorkloadProcessLatencyPercentile percentile : latency) {
+                MetricNameDto dto = new MetricNameDto();
+                dto.setName("Latency "+Double.toString(percentile.getPercentileKey())+" %");
+                dto.setTests(tests);
+                latencyNames.add(dto);
             }
-        }catch (Exception ex){
-            ex.printStackTrace();
         }
         return latencyNames;
     }
