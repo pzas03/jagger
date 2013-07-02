24dc4eaecb4ba47914fffacce40982be21039870
Kirill Popov <kpopov@griddynamics.com>
2013-07-02 17:03:16 +0400

    [JFG-400]: Quoted $@ to allow multi-word parameters

--- distribution/src/main/resources/start.sh
+++ distribution/src/main/resources/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 FIRST_ARG="$1" # save the first argument because it will be not available after `shift`
 shift # required to pass all positional arguments except first one
-echo $JAVA_HOME/bin/java -Xmx2550m -Xms2550m $@ -classpath "./modules/chassis/*:./modules/diagnostics/*:./lib/*:./configuration/boot/" -Dlog4j.configuration=jagger.log4j.properties com.griddynamics.jagger.JaggerLauncher $FIRST_ARG
-$JAVA_HOME/bin/java -Xmx2550m -Xms2550m $@ -classpath "./modules/chassis/*:./modules/diagnostics/*:./lib/*:./configuration/boot/" -Dlog4j.configuration=jagger.log4j.properties com.griddynamics.jagger.JaggerLauncher $FIRST_ARG
+echo $JAVA_HOME/bin/java -Xmx2550m -Xms2550m "$@" -classpath "./modules/chassis/*:./modules/diagnostics/*:./lib/*:./configuration/boot/" -Dlog4j.configuration=jagger.log4j.properties com.griddynamics.jagger.JaggerLauncher $FIRST_ARG
+$JAVA_HOME/bin/java -Xmx2550m -Xms2550m "$@" -classpath "./modules/chassis/*:./modules/diagnostics/*:./lib/*:./configuration/boot/" -Dlog4j.configuration=jagger.log4j.properties com.griddynamics.jagger.JaggerLauncher $FIRST_ARG
