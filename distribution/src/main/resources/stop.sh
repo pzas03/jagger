742cc0e1b5fed7a83638c281946c1c448058480d
Kirill Popov <kpopov@griddynamics.com>
2013-07-02 12:57:17 +0400

    More reliable Jagger process kill'ing
    +simpler way of passing arguments

--- distribution/src/main/resources/stop.sh
+++ distribution/src/main/resources/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # deleting all processes with specified name
-ps axwww | grep JaggerLauncher | awk '{print $1}' | xargs -n1 kill -9
+pgrep -f JaggerLauncher | xargs -n1 kill -9
 
--- distribution/src/main/resources/stop_agent.sh
+++ distribution/src/main/resources/stop_agent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # deleting all processes with specified name
-ps axwww | grep AgentStarter | awk '{print $1}' | xargs -n1 kill -9
+pgrep -f AgentStarter | xargs -n1 kill -9
 
