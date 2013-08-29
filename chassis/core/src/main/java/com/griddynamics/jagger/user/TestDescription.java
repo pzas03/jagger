ff347d04cf044e1db6cff1d0382159105267e32e
kgribov <kgribov@griddynamics.com>
2013-08-29 13:45:12 +0400

    Fix parsing name attribute in testDescription

--- chassis/core/src/main/java/com/griddynamics/jagger/user/TestDescription.java
+++ chassis/core/src/main/java/com/griddynamics/jagger/user/TestDescription.java
@@ -26,6 +26,7 @@ public class TestDescription {
     private Calibrator calibrator = new OneNodeCalibrator();
     private String description = "";
     private String version;
+    private String name;
 
     public List<KernelSideObjectProvider<ScenarioCollector<Object,Object,Object>>> getStandardCollectors() {
         return standardCollectors;
@@ -83,11 +84,21 @@ public class TestDescription {
         this.version = version;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public WorkloadTask generatePrototype(){
         WorkloadTask prototype = new WorkloadTask();
         prototype.setCalibrator(calibrator);
         prototype.setDescription(description);
         prototype.setScenarioFactory(scenarioFactory);
+        prototype.setName(name);
+        prototype.setVersion(version);
 
         List<KernelSideObjectProvider<ScenarioCollector<Object,Object,Object>>> listeners = new ArrayList<KernelSideObjectProvider<ScenarioCollector<Object,Object,Object>>>(metrics.size()+ standardCollectors.size()+1);
         listeners.addAll(standardCollectors);
