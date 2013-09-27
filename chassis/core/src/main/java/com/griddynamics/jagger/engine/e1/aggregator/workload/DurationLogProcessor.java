c56660d423fa17ec378960d48806daafc3271267
kgribov <kgribov@griddynamics.com>
2013-09-27 15:09:05 +0400

    Try to refactor LogProcessors

--- chassis/core/src/main/java/com/griddynamics/jagger/engine/e1/aggregator/workload/DurationLogProcessor.java
+++ chassis/core/src/main/java/com/griddynamics/jagger/engine/e1/aggregator/workload/DurationLogProcessor.java
@@ -1,22 +1,22 @@
 /*
- * Copyright (c) 2010-2012 Grid Dynamics Consulting Services, Inc, All Rights Reserved
- * http://www.griddynamics.com
- *
- * This library is free software; you can redistribute it and/or modify it under the terms of
- * the GNU Lesser General Public License as published by the Free Software Foundation; either
- * version 2.1 of the License, or any later version.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+* Copyright (c) 2010-2012 Grid Dynamics Consulting Services, Inc, All Rights Reserved
+* http://www.griddynamics.com
+*
+* This library is free software; you can redistribute it and/or modify it under the terms of
+* the GNU Lesser General Public License as published by the Free Software Foundation; either
+* version 2.1 of the License, or any later version.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 package com.griddynamics.jagger.engine.e1.aggregator.workload;
 
@@ -47,9 +47,9 @@ import java.util.Collection;
 import java.util.Iterator;
 
 /**
- * @author Alexey Kiselyov
- *         Date: 20.07.11
- */
+* @author Alexey Kiselyov
+*         Date: 20.07.11
+*/
 public class DurationLogProcessor extends LogProcessor implements DistributionListener {
 
     private static final Logger log = LoggerFactory.getLogger(Master.class);
@@ -171,18 +171,18 @@ public class DurationLogProcessor extends LogProcessor implements DistributionLi
 
                     log.debug("Log entry {} time", logEntry.getTime());
 
+                    int countInterval = intervalSize;
                     while (logEntry.getTime() > currentInterval) {
                         log.debug("processing count {} interval {}", currentCount, intervalSize);
 
                         if (currentCount > 0) {
-                            double throughput = (double) currentCount * 1000 / intervalSize;
+                            double throughput = (double) currentCount * 1000 / countInterval;
                             statistics.add(assembleInvocationStatistics(time, windowStatisticsCalculator, throughput, taskData));
                             currentCount = 0;
                             windowStatisticsCalculator.reset();
-                        } else {
-                            statistics.add(new TimeInvocationStatistics(time, 0d, 0d, 0d, taskData));
                         }
                         time += intervalSize;
+                        countInterval += intervalSize;
                         currentInterval += intervalSize;
                     }
                     currentCount++;
