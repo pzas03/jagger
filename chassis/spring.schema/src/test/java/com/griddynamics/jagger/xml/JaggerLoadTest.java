1d576215d01e5361d6f317f27b36951db58098e6
kgribov <kgribov@griddynamics.com>
2014-01-13 18:27:55 +0400

    Fix conflicts after merge

--- chassis/spring.schema/src/test/java/com/griddynamics/jagger/xml/JaggerLoadTest.java
+++ chassis/spring.schema/src/test/java/com/griddynamics/jagger/xml/JaggerLoadTest.java
@@ -36,54 +36,6 @@ public class JaggerLoadTest {
     }
 
     @Test
-    public void testTpsTask(){
-        TpsClockConfiguration tpsClock = (TpsClockConfiguration)context.getBean("tps1");
-        Assert.assertEquals(tpsClock.getTickInterval(), 1000);
-        Assert.assertEquals(tpsClock.getTps(), 200d);
-
-        tpsClock = (TpsClockConfiguration)context.getBean("tps2");
-        Assert.assertEquals(tpsClock.getTickInterval(), 500);
-        Assert.assertEquals(tpsClock.getTps(), 100d);
-    }
-
-    @Test
-    public void testInvocationTask(){
-        ExactInvocationsClockConfiguration invClock = (ExactInvocationsClockConfiguration)context.getBean("inv1");
-        Assert.assertEquals(invClock.getTickInterval(), 1000);
-        Assert.assertEquals(invClock.getSamplesCount(), 100);
-        Assert.assertEquals(invClock.getThreadCount(), 1);
-        Assert.assertEquals(invClock.getDelay(), 100);
-
-        invClock = (ExactInvocationsClockConfiguration)context.getBean("inv2");
-        Assert.assertEquals(invClock.getTickInterval(), 500);
-        Assert.assertEquals(invClock.getSamplesCount(), 50);
-        Assert.assertEquals(invClock.getThreadCount(), 2);
-        Assert.assertEquals(invClock.getDelay(), 0);
-
-    }
-
-    @Test
-    public void testThreadsDelay(){
-        VirtualUsersClockConfiguration invClock = (VirtualUsersClockConfiguration)context.getBean("load_threads_with_delay");
-        Assert.assertEquals(1101, invClock.getDelay().getInvocationDelay().getValue());
-
-    }
-
-    @Test
-    public void testTerminationStrategy(){
-        IterationsOrDurationStrategyConfiguration conf = (IterationsOrDurationStrategyConfiguration) context.getBean("ts1");
-        Assert.assertEquals(conf.getIterations(), -1);
-        Assert.assertEquals(conf.getDuration(), "2h");
-
-        conf = (IterationsOrDurationStrategyConfiguration) context.getBean("ts2");
-        Assert.assertEquals(conf.getIterations(), 5);
-        Assert.assertEquals(conf.getDuration(), null);
-
-        InfiniteTerminationStrategyConfiguration  conf1 = (InfiniteTerminationStrategyConfiguration) context.getBean("ts3");
-
-    }
-
-    @Test
     public void testNewTest(){
         TestConfiguration testConfiguration = (TestConfiguration) context.getBean("test1");
 
