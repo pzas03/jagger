2bec90c03e09cc4cdfcb8e51a5bfa42aad1fbdae
amikryukov <amikryukov@griddynamics.com>
2014-05-05 15:15:58 +0400

    + calculate number of columns on fly

--- webclient/src/main/java/com/griddynamics/jagger/webclient/client/components/PlotSaver.java
+++ webclient/src/main/java/com/griddynamics/jagger/webclient/client/components/PlotSaver.java
@@ -19,94 +19,116 @@ public class PlotSaver {
      * @param xAxisLabel X axis label
      */
     public void saveAsPng (SimplePlot plot, String plotHeader, String xAxisLabel) {
-        saveAsPng(plot, plotHeader, xAxisLabel, 2);
-    }
-
-    /**
-     * Saves plot as png
-     * @param plot plot to be saved
-     * @param plotHeader plot header
-     * @param xAxisLabel X axis label
-     * @param numberOfColumns number of columns in legend
-     */
-    public void saveAsPng (SimplePlot plot, String plotHeader, String xAxisLabel, int numberOfColumns) {
         if (plot.isExportAsImageEnabled()) {
 
-            ImageElement imageElement = ImageElement.as(plot.getImage().getElement());
-            int plotWidth = imageElement.getWidth();
-            int plotHeight = imageElement.getHeight();
-            int fontSize = 13;
-            int delta = 5;
+            Canvas canvasTmp = Canvas.createIfSupported();
+            if (canvasTmp == null) {
+                alert("Can not create canvas element in your browser.");
+                return;
+            }
 
+            Context2d context = canvasTmp.getContext2d();
+
+            ImageElement imageElement = ImageElement.as(plot.getImage().getElement());
+
+            // Width of plot image
+            int plotWidth = imageElement.getWidth();
+
+            // Height of plot image
+            int plotHeight = imageElement.getHeight();
+
+            // Font size of text in pixels
+            int fontSize = 13;
+
+            // Indent size in pixels. Used to separate all elements on final image and defines left indent of header or legend
+            int indent = 5;
 
             JsArray<Series> series = plot.getModel().getSeries();
-            int legendHeight = calculateLegendHeight(series, numberOfColumns, fontSize, delta);
 
-            int totalHeight = plotHeight + 2 * fontSize + 3 * delta + legendHeight;
+            // Calculate max width of legend label to avoid overlay
+            int maxLegendLabelWidth = calculateLabelMaxWidth(series, context, fontSize);
 
-            Canvas canvasTmp = Canvas.createIfSupported();
+            int legendRectangleWidth = (int)(fontSize * 1.5);
+            int legendRectangleHeight = fontSize;
+
+            // Total width of one series legend including rectangle with color, and indents.
+            int oneSeriesLegendSize = legendRectangleWidth + 3 * indent + maxLegendLabelWidth;
+
+            // Number of columns in legend
+            int numberOfColumns = plotWidth / oneSeriesLegendSize;
+
+            if (numberOfColumns == 0)
+                numberOfColumns = 1;
+
+            int legendHeight = calculateLegendHeight(series, numberOfColumns, fontSize, indent);
+
+            // 2 * fontSize as we have Header of plot and X axis label
+            // 3 * indent as we have indent from top to header,
+            //                       indent between plot and header,
+            //                       indent between plot and X axis label.
+            int totalHeight = plotHeight + 2 * fontSize + 3 * indent + legendHeight;
+
 
             canvasTmp.setWidth(plotWidth + "px");
             canvasTmp.setHeight(totalHeight +  "px");
             canvasTmp.setCoordinateSpaceWidth(plotWidth);
             canvasTmp.setCoordinateSpaceHeight(totalHeight);
-            Context2d context = canvasTmp.getContext2d();
+
 
             int currentY = 0;
             context.setFillStyle("black");
             context.setFont("bold " + fontSize + "px sans-serif");
             context.setTextAlign(Context2d.TextAlign.START);
 
-            currentY += fontSize + delta;
+            currentY += fontSize + indent;
             // add plot header
-            context.fillText(plotHeader, delta, currentY);
+            context.fillText(plotHeader, indent, currentY);
 
-            currentY += delta;
+            currentY += indent;
             // add image of plot
             context.drawImage(imageElement, 0.0, currentY, imageElement.getWidth(), imageElement.getHeight());
 
-            currentY += plotHeight + delta;
+            currentY += plotHeight + indent;
             context.setFont(fontSize + "px sans-serif");
             context.setTextAlign(Context2d.TextAlign.CENTER);
             // add x axis label
             context.fillText(xAxisLabel, imageElement.getWidth() / 2, currentY + fontSize / 2);
 
-            currentY += fontSize + delta;
+            currentY += fontSize + indent;
 
-            // calculate max width of legend label to avoid overlay
-            int maxWidth = calculateLabelMaxWidth(series, context, fontSize);
+
 
             int currentX = 0;
             for (int i = 0; i < series.length(); i++) {
 
                 Series s = series.get(i);
-                int recW = (int)(fontSize * 1.5);
-                int recH = fontSize;
 
+                int curRecW = legendRectangleWidth;
+                int curRecH = legendRectangleHeight;
                 if (i % numberOfColumns == 0 ) {
-                    currentX = delta;
+                    currentX = indent;
                 } else {
-                    currentX += (maxWidth + 2 * fontSize + 3 * delta);
+                    currentX += indent + oneSeriesLegendSize;
                 }
 
                 // add rectangle with color
                 context.setFillStyle("gray");
-                context.fillRect(currentX, currentY, recW, recH);
+                context.fillRect(currentX, currentY, curRecW, curRecH);
                 context.setFillStyle("white");
-                recW -= 2;
-                recH -= 2;
-                context.fillRect(currentX + 1, currentY + 1, recW, recH);
+                curRecW -= 2;
+                curRecH -= 2;
+                context.fillRect(currentX + 1, currentY + 1, curRecW, curRecH);
                 context.setFillStyle(s.getColor());
-                recW -= 2;
-                recH -= 2;
-                context.fillRect(currentX + 2, currentY + 2, recW, recH);
+                curRecW -= 2;
+                curRecH -= 2;
+                context.fillRect(currentX + 2, currentY + 2, curRecW, curRecH);
                 context.setFillStyle("black");
                 context.setTextAlign(Context2d.TextAlign.START);
                 // add label of current series
-                context.fillText(s.getLabel(), currentX + delta * 2 + fontSize * 1.5, currentY + fontSize - 2);
+                context.fillText(s.getLabel(), currentX + legendRectangleWidth + indent, currentY + fontSize - 2);
 
                 if (i % numberOfColumns == numberOfColumns - 1) {
-                    currentY += fontSize + delta;
+                    currentY += fontSize + indent;
                 }
             }
 
@@ -116,8 +138,12 @@ public class PlotSaver {
             Window.Location.assign(url.replace("image/png", "image/octet-stream"));
 
         } else {
-            new ExceptionPanel("Can not save image in your browser.");
+            alert("Can not save image in your browser.");
         }
+    }
+
+    private void alert(String message) {
+        new ExceptionPanel(message);
     }
 
     private int calculateLegendHeight(JsArray<Series> series, int numberOfColumns, int fontSize, int delta) {
