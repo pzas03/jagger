0775a00b2819e4098ef48c698aa29ea205576934
marknovozhilov <mnovozhilov@griddynamics.com>
2014-06-02 18:57:08 +0400

    Update HibernateKeyValueStorage.java

--- chassis/core/src/main/java/com/griddynamics/jagger/storage/rdb/HibernateKeyValueStorage.java
+++ chassis/core/src/main/java/com/griddynamics/jagger/storage/rdb/HibernateKeyValueStorage.java
@@ -55,7 +55,7 @@ public class HibernateKeyValueStorage extends HibernateDaoSupport implements Key
     @Required
     public void setSessionTempDataCount(int sessionTempDataCount) {
         if (sessionTempDataCount <0) {
-            log.warn("Session count can't be < 0; user setting chassis.storage.temporary.data.session.count in {}.", sessionTempDataCount);
+            log.warn("Session count can't be < 0; chassis.storage.temporary.data.session.count is equal {}.", sessionTempDataCount);
             return;
         }
         this.sessionTempDataCount = sessionTempDataCount;
