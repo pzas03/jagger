519acd546c3613d1b64917a3f499a8a8d26939d2
marknovozhilov <mnovozhilov@griddynamics.com>
2014-06-18 19:14:09 +0400

    deleted unused "imports"

--- dbapi/src/main/java/com/griddynamics/jagger/dbapi/fetcher/PlotsDbMetricDataFetcher.java
+++ dbapi/src/main/java/com/griddynamics/jagger/dbapi/fetcher/PlotsDbMetricDataFetcher.java
@@ -1,18 +1,12 @@
 package com.griddynamics.jagger.dbapi.fetcher;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
 import com.griddynamics.jagger.dbapi.dto.MetricNameDto;
 import com.griddynamics.jagger.dbapi.dto.PlotDatasetDto;
 import com.griddynamics.jagger.dbapi.util.LegendProvider;
 import com.griddynamics.jagger.util.Pair;
-
 import org.springframework.beans.factory.annotation.Required;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 
 public abstract class PlotsDbMetricDataFetcher extends DbMetricDataFetcher<Pair<MetricNameDto, List<PlotDatasetDto>>> {
