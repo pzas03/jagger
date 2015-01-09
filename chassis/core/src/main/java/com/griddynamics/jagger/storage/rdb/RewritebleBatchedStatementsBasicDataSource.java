6ba953a27a8b83cabbd8c6c2ea7dc7d7517a8c78
Artem Zhdanov <artem.zhdanov@macys.com>
2015-01-09 17:24:52 +0200

    H2 jdbc driver does not support '?' symbol in JDBC url string. Fixed.

---
+++ chassis/core/src/main/java/com/griddynamics/jagger/storage/rdb/RewritebleBatchedStatementsBasicDataSource.java
@@ -0,0 +1,35 @@
+package com.griddynamics.jagger.storage.rdb;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+import java.sql.SQLException;
+
+/**
+ * @author Artem Zhdanov <azhdanov@griddynamics.com>
+ * @since 08/01/2015
+ */
+public class RewritebleBatchedStatementsBasicDataSource extends BasicDataSource implements RewritebleBatchedStatementsDataSource {
+
+    public static final String REWRITE_BATCHED_STATEMENTS = "rewriteBatchedStatements";
+
+    @Override
+    public boolean getRewriteBatchedStatements() {
+        final String isRewrite = super.connectionProperties.getProperty(REWRITE_BATCHED_STATEMENTS);
+        return isRewrite == null ? null : Boolean.valueOf(isRewrite);
+    }
+
+    @Override
+    public void setRewriteBatchedStatements(final boolean rewriteBatchedStatements) {
+        super.addConnectionProperty("includeSynonyms", String.valueOf(rewriteBatchedStatements));
+    }
+
+    @Override
+    public <T> T unwrap(final Class<T> iface) throws SQLException {
+        throw new UnsupportedOperationException("Is not implemented by org.apache.commons.dbcp.BasicDataSource so we will not implement it either");
+    }
+
+    @Override
+    public boolean isWrapperFor(final Class<?> iface) throws SQLException {
+        throw new UnsupportedOperationException("Is not implemented by org.apache.commons.dbcp.BasicDataSource so we will not implement it either");
+    }
+}
