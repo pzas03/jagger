aba433a7fc5ce8b8c9917e4e34e67fe420fe2bc6
anton-antonenko <kingtoxa@gmail.com>
2016-11-07 15:05:45 +0200

    Add swagger docs for Envs API

--- jaas/src/main/java/com/griddynamics/jagger/jaas/rest/TestEnvironmentRestController.java
+++ jaas/src/main/java/com/griddynamics/jagger/jaas/rest/TestEnvironmentRestController.java
@@ -5,8 +5,14 @@ import com.griddynamics.jagger.jaas.exceptions.ResourceNotFoundException;
 import com.griddynamics.jagger.jaas.exceptions.TestEnvironmentInvalidIdException;
 import com.griddynamics.jagger.jaas.exceptions.TestEnvironmentNoSessionException;
 import com.griddynamics.jagger.jaas.exceptions.TestEnvironmentSessionNotFoundException;
+import com.griddynamics.jagger.jaas.exceptions.WrongTestEnvironmentStatusException;
 import com.griddynamics.jagger.jaas.service.TestEnvironmentService;
 import com.griddynamics.jagger.jaas.storage.model.TestEnvironmentEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ResponseHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -21,10 +27,13 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.griddynamics.jagger.jaas.storage.model.TestEnvironmentEntity.TestEnvironmentStatus.PENDING;
+import static com.griddynamics.jagger.jaas.storage.model.TestEnvironmentEntity.TestEnvironmentStatus.RUNNING;
 import static java.time.Instant.ofEpochMilli;
 import static java.time.ZoneOffset.UTC;
 import static java.time.ZonedDateTime.ofInstant;
@@ -35,6 +44,8 @@ import static org.springframework.web.servlet.support.ServletUriComponentsBuilde
 
 @RestController
 @RequestMapping(value = "/envs")
+@Api(description = "This is the API for Jagger Test Environments. It provides endpoints for reading, creating and updating Test Environments. "
+        + "Deleting is performed automatically by cleaning job. Expiration time of environments is set by property 'environments.ttl.minutes'.")
 public class TestEnvironmentRestController extends AbstractController {
 
     private static final String ENVIRONMENT_SESSION_COOKIE = "Environment-Session";
@@ -52,6 +63,11 @@ public class TestEnvironmentRestController extends AbstractController {
     }
 
     @GetMapping(value = "/{envId}", produces = APPLICATION_JSON_VALUE)
+    @ApiOperation(response = TestEnvironmentEntity.class, value = "Retrieves Test Environment by envId.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Test Environment is found.", responseHeaders = @ResponseHeader(name = "Environment-Expires",
+                    description = "This header stores expiration time of Test Environment.", response = Date.class)),
+            @ApiResponse(code = 404, message = "Test Environment with provided envId is not found.")})
     public ResponseEntity<TestEnvironmentEntity> getTestEnvironment(@PathVariable String envId, HttpServletResponse response) {
         ResponseEntity<TestEnvironmentEntity> responseEntity = produceGetResponse(testEnvService, function -> testEnvService.read(envId));
         if (responseEntity.getStatusCode() == OK)
@@ -60,11 +76,19 @@ public class TestEnvironmentRestController extends AbstractController {
     }
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
+    @ApiOperation(response = TestEnvironmentEntity.class, responseContainer = "List", value = "Retrieves all Test Environments.")
     public ResponseEntity<List<TestEnvironmentEntity>> getTestEnvironments() {
         return produceGetResponse(testEnvService, function -> testEnvService.readAll());
     }
 
     @PutMapping(value = "/{envId}", consumes = APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Updates Test Environment by envId.", notes = "This operation can be performed only if 'Environment-Session' cookie is "
+            + "present and valid. To obtain this cookie POST to /envs must be performed firstly. This cookie is valid only for Test Environment with "
+            + "envId which was specified in POST request body.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 202, message = "Update is successful."),
+            @ApiResponse(code = 404, message = "Test Environment with provided envId or sessionId is not found."),
+            @ApiResponse(code = 400, message = "Environment-Session cookie is not present.")})
     public ResponseEntity<?> updateTestEnvironment(@PathVariable String envId, @RequestBody TestEnvironmentEntity testEnv,
                                                    HttpServletRequest request, HttpServletResponse response) {
         if (!testEnvService.exists(envId))
@@ -84,24 +108,33 @@ public class TestEnvironmentRestController extends AbstractController {
     }
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Creates Test Environment.", notes = "If operation successful, 'Environment-Session' cookie is returned with response, "
+            + "which is required for PUT request. This cookie is valid only for Test Environment with envId which was specified in request body.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Creation is successful."),
+            @ApiResponse(code = 400, message = "envId doesn't match " + ENV_ID_PATTERN + " OR status doesn't correspond to runningTestSuite value."),
+            @ApiResponse(code = 409, message = "Test Environment with provided envId already exists.")})
     public ResponseEntity<?> createTestEnvironment(@RequestBody TestEnvironmentEntity testEnv, HttpServletResponse response) {
-        validateTestEnvId(testEnv);
+        validateTestEnv(testEnv);
         if (testEnvService.exists(testEnv.getEnvironmentId()))
             throw new ResourceAlreadyExistsException("Test Environment", testEnv.getEnvironmentId());
 
         TestEnvironmentEntity created = testEnvService.create(testEnv);
         setExpiresHeader(response, created);
         setSessionCookie(response, created, environmentsTtlMinutes * 60);
-        return ResponseEntity.created(fromCurrentRequest().path("/{envId}")
-                .buildAndExpand(testEnv.getEnvironmentId()).toUri()).build();
+        return ResponseEntity.created(fromCurrentRequest().path("/{envId}").buildAndExpand(testEnv.getEnvironmentId()).toUri()).build();
     }
 
-    private void validateTestEnvId(TestEnvironmentEntity testEnv) {
+    private void validateTestEnv(TestEnvironmentEntity testEnv) {
         String envId = testEnv.getEnvironmentId();
         Pattern envIdPattern = Pattern.compile(ENV_ID_PATTERN);
         Matcher matcher = envIdPattern.matcher(envId);
         if (!matcher.matches())
             throw new TestEnvironmentInvalidIdException(envId, envIdPattern);
+
+        if (testEnv.getRunningTestSuite() != null && testEnv.getStatus() == RUNNING
+                || testEnv.getRunningTestSuite() == null && testEnv.getStatus() == PENDING)
+            throw new WrongTestEnvironmentStatusException(testEnv.getStatus(), testEnv.getRunningTestSuite());
     }
 
     private void setExpiresHeader(HttpServletResponse response, TestEnvironmentEntity testEnv) {
