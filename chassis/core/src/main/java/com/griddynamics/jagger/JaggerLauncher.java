1feb9138402431eef23e28459f1654843dcc0b3a
Andrey Badaev <abadaev@griddynamics.com>
2016-11-07 12:14:05 +0300

    [JFG-948] removed Master REST API feature (#664)

--- chassis/core/src/main/java/com/griddynamics/jagger/JaggerLauncher.java
+++ chassis/core/src/main/java/com/griddynamics/jagger/JaggerLauncher.java
@@ -32,20 +32,15 @@ import com.griddynamics.jagger.storage.rdb.H2DatabaseServer;
 import com.griddynamics.jagger.util.JaggerXmlApplicationContext;
 import org.apache.commons.lang.StringUtils;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.XmlWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
 import com.google.common.collect.Sets;
 
@@ -61,8 +56,6 @@ import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 
 public final class JaggerLauncher {
-    private static final Logger log = LoggerFactory.getLogger(JaggerLauncher.class);
-
     public static final String ROLES = "chassis.roles";
     public static final String MASTER_CONFIGURATION = "chassis.master.configuration";
     public static final String REPORTER_CONFIGURATION = "chassis.reporter.configuration";
@@ -72,29 +65,31 @@ public final class JaggerLauncher {
     public static final String RDB_CONFIGURATION = "chassis.rdb.configuration";
     public static final String INCLUDE_SUFFIX = ".include";
     public static final String EXCLUDE_SUFFIX = ".exclude";
-    
     public static final String TEST_CONFIG_NAME_PROP = "chassis.master.session.configuration.bean.name";
-
     public static final String DEFAULT_ENVIRONMENT_PROPERTIES = "jagger.default.environment.properties";
     public static final String USER_ENVIRONMENT_PROPERTIES = "jagger.user.environment.properties";
     public static final String ENVIRONMENT_PROPERTIES = "jagger.environment.properties";
-
-
-    private static final String DEFAULT_ENVIRONMENT_PROPERTIES_LOCATION = "./configuration/basic/default.environment.properties";
-    private static final String DEFAULT_USER_ENVIRONMENT_PROPERTIES_LOCATION = "./configuration/basic/default.user.properties";
-
+    private static final Logger log = LoggerFactory.getLogger(JaggerLauncher.class);
+    private static final String DEFAULT_ENVIRONMENT_PROPERTIES_LOCATION =
+            "./configuration/basic/default.environment.properties";
+    private static final String DEFAULT_USER_ENVIRONMENT_PROPERTIES_LOCATION =
+            "./configuration/basic/default.user.properties";
+    
     private static final Properties environmentProperties = new Properties();
-
+    
     private static final Launches.LaunchManagerBuilder builder = Launches.builder();
-
+    
     public static void main(String[] args) throws Exception {
         Thread memoryMonitorThread = new Thread("memory-monitor") {
             @Override
             public void run() {
-                for (;;) {
+                for (; ; ) {
                     try {
-                        log.info("Memory info: totalMemory={}, freeMemory={}", Runtime.getRuntime().totalMemory(), Runtime.getRuntime().freeMemory());
-
+                        log.info(
+                                "Memory info: totalMemory={}, freeMemory={}", Runtime.getRuntime().totalMemory(),
+                                Runtime.getRuntime().freeMemory()
+                        );
+                        
                         Thread.sleep(60000);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
@@ -104,27 +99,27 @@ public final class JaggerLauncher {
         };
         memoryMonitorThread.setDaemon(true);
         memoryMonitorThread.start();
-
+        
         String pid = ManagementFactory.getRuntimeMXBean().getName();
         System.out.println(String.format("PID:%s", pid));
-
+        
         Properties props = System.getProperties();
         for (Map.Entry<Object, Object> prop : props.entrySet()) {
             log.info("{}: '{}'", prop.getKey(), prop.getValue());
         }
         log.info("");
-
+        
         URL directory = new URL("file:" + System.getProperty("user.dir") + "/");
         loadBootProperties(directory, args[0], environmentProperties);
-
+        
         log.debug("Bootstrap properties:");
         for (String propName : environmentProperties.stringPropertyNames()) {
             log.debug("   {}={}", propName, environmentProperties.getProperty(propName));
         }
-
+        
         String[] roles = environmentProperties.getProperty(ROLES).split(",");
         Set<String> rolesSet = Sets.newHashSet(roles);
-
+        
         if (rolesSet.contains(Role.COORDINATION_SERVER.toString())) {
             launchCoordinationServer(directory);
         }
@@ -140,11 +135,11 @@ public final class JaggerLauncher {
         if (rolesSet.contains(Role.KERNEL.toString())) {
             launchKernel(directory);
         }
-
+        
         if (rolesSet.contains(Role.REPORTER.toString())) {
             launchReporter(directory);
         }
-
+        
         LaunchManager launchManager = builder.build();
         int result = launchManager.launch();
         System.exit(result);
@@ -155,7 +150,7 @@ public final class JaggerLauncher {
         coordinator.waitForReady();
         coordinator.initialize();
     }
-
+    
     private static void launchMaster(final URL directory) {
         LaunchTask masterTask = new LaunchTask() {
             @Override
@@ -166,40 +161,24 @@ public final class JaggerLauncher {
                 if (isStandByMode) {
                     log.info("Starting Master in stand by mode...");
                     while (isStandByMode) {
-                       environmentProperties.setProperty(TEST_CONFIG_NAME_PROP, getTestConfigName());
-                       XmlWebApplicationContext context =
-                               (XmlWebApplicationContext) loadContext(directory, MASTER_CONFIGURATION, environmentProperties);
-                       initCoordinator(context);
-                       Master master = (Master) context.getBean("master");
-                       master.run();
-                       
-                       context.destroy();
+                        environmentProperties.setProperty(TEST_CONFIG_NAME_PROP, getTestConfigName());
+                        doLaunchMaster(directory);
                     }
                 } else {
                     log.info("Starting Master in right away mode...");
-                    WebApplicationContext context = loadContext(directory, MASTER_CONFIGURATION, environmentProperties);
-                    Server server = new Server(getPortFrom("master.rest.http.port", 9090));
-                    try {
-                        server.setHandler(getServletContextHandler(context));
-                        server.start();
-    
-                        initCoordinator(context);
-                        Master master = (Master) context.getBean("master");
-                        master.run();
-                    } catch (Exception e) {
-                        log.error("Error during embedded Jetty handling.", e);
-                        throw new RuntimeException(e);
-                    } finally {
-                        try {
-                            server.stop();
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
+                    doLaunchMaster(directory);
                 }
             }
         };
         builder.addMainTask(masterTask);
+    }
+    
+    private static void doLaunchMaster(final URL directory) {
+        AbstractXmlApplicationContext context = loadContext(directory, MASTER_CONFIGURATION, environmentProperties);
+        initCoordinator(context);
+        Master master = (Master) context.getBean("master");
+        master.run();
+        context.destroy();
     }
     
     private static String getTestConfigName() {
@@ -210,25 +189,9 @@ public final class JaggerLauncher {
         return environmentProperties.getProperty(TEST_CONFIG_NAME_PROP);
     }
     
-    private static int getPortFrom(String envPropName, int def) {
-        int port = def;
-        try {
-            port = Integer.parseInt(environmentProperties.getProperty(envPropName));
-        } finally {
-            return port;
-        }
-    }
-    
-    private static ServletContextHandler getServletContextHandler(WebApplicationContext context) {
-        ServletContextHandler contextHandler = new ServletContextHandler();
-        contextHandler.addServlet(new ServletHolder(new DispatcherServlet(context)), "/jaas/*");
-        contextHandler.addEventListener(new ContextLoaderListener(context));
-        return contextHandler;
-    }
-
     private static void launchReporter(final URL directory) {
         LaunchTask launchReporter = new LaunchTask() {
-
+            
             @Override
             public void run() {
                 ApplicationContext context = loadContext(directory, REPORTER_CONFIGURATION, environmentProperties);
@@ -236,28 +199,28 @@ public final class JaggerLauncher {
                 reportingService.renderReport(true);
             }
         };
-
+        
         builder.addMainTask(launchReporter);
     }
-
+    
     private static void launchKernel(final URL directory) {
-
+        
         LaunchTask runKernel = new LaunchTask() {
             private Kernel kernel;
-
+            
             @Override
             public void run() {
                 log.info("Starting Kernel");
-
+                
                 ApplicationContext context = loadContext(directory, KERNEL_CONFIGURATION, environmentProperties);
-
+                
                 final CountDownLatch latch = new CountDownLatch(1);
                 final Coordinator coordinator = (Coordinator) context.getBean("coordinator");
-
+                
                 kernel = (Kernel) context.getBean("kernel");
-
+                
                 toTerminate(kernel);
-
+                
                 Runnable kernelRunner = () -> {
                     try {
                         latch.await();
@@ -266,66 +229,66 @@ public final class JaggerLauncher {
                     }
                     kernel.run();
                 };
-
+                
                 getExecutor().execute(kernelRunner);
-
+                
                 coordinator.waitForReady();
                 coordinator.waitForInitialization();
-
+                
                 latch.countDown();
             }
         };
-
+        
         builder.addBackgroundTask(runKernel);
     }
-
+    
     private static void launchRdbServer(final URL directory) {
         log.info("Starting RDB Server");
-
-
+        
+        
         LaunchTask rdbRunner = new LaunchTask() {
             @Override
             public void run() {
                 ApplicationContext context = loadContext(directory, RDB_CONFIGURATION, environmentProperties);
-
+                
                 H2DatabaseServer dbServer = (H2DatabaseServer) context.getBean("databaseServer");
-
+                
                 dbServer.run();
             }
         };
-
+        
         builder.addBackgroundTask(rdbRunner);
     }
-
+    
     private static void launchCoordinationServer(final URL directory) {
         LaunchTask zookeeperInitializer = new LaunchTask() {
-
+            
             //            private ZooKeeperServer zooKeeper;
             private AttendantServer server;
-
+            
             public void run() {
                 log.info("Starting Coordination Server");
-
+                
                 ApplicationContext context = loadContext(directory, COORDINATION_CONFIGURATION, environmentProperties);
                 server = (AttendantServer) context.getBean("coordinatorServer");
                 toTerminate(server);
                 getExecutor().execute(server);
                 server.initialize();
             }
-
         };
-
+        
         builder.addMainTask(zookeeperInitializer);
     }
-
+    
     private static void launchCometdCoordinationServer(final URL directory) {
-
+        
         LaunchTask jettyRunner = new LaunchTask() {
             public void run() {
                 log.info("Starting Cometd Coordination Server");
-
-                ApplicationContext context = loadContext(directory, COORDINATION_HTTP_CONFIGURATION, environmentProperties);
-    
+                
+                ApplicationContext context =
+                        loadContext(directory, COORDINATION_HTTP_CONFIGURATION, environmentProperties);
+                
                 initCoordinator(context);
                 
                 Server jettyServer = (Server) context.getBean("jettyServer");
@@ -339,21 +302,23 @@ public final class JaggerLauncher {
         builder.addMainTask(jettyRunner);
     }
     
-    public static WebApplicationContext loadContext(URL directory, String role, Properties environmentProperties) {
+    public static AbstractXmlApplicationContext loadContext(URL directory, String role, Properties environmentProperties) {
         String[] includePatterns = StringUtils.split(environmentProperties.getProperty(role + INCLUDE_SUFFIX), ", ");
         String[] excludePatterns = StringUtils.split(environmentProperties.getProperty(role + EXCLUDE_SUFFIX), ", ");
-
+        
         List<String> descriptors = discoverResources(directory, includePatterns, excludePatterns);
         log.info("Discovered descriptors:");
         for (String descriptor : descriptors) {
             log.info("   " + descriptor);
         }
         
-        return new JaggerXmlApplicationContext(directory, environmentProperties, descriptors.toArray(new String[descriptors.size()]));
+        return new JaggerXmlApplicationContext(
+                directory, environmentProperties, descriptors.toArray(new String[descriptors.size()]));
     }
-
+    
     private static List<String> discoverResources(URL directory, String[] includePatterns, String[] excludePatterns) {
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(new FileSystemResourceLoader());
+        PathMatchingResourcePatternResolver resolver =
+                new PathMatchingResourcePatternResolver(new FileSystemResourceLoader());
         List<String> resourceNames = new ArrayList<>();
         PathMatcher matcher = new AntPathMatcher();
         try {
@@ -375,20 +340,22 @@ public final class JaggerLauncher {
         } catch (IOException e) {
             throw new TechnicalException(e);
         }
-
+        
         return resourceNames;
     }
-
-    public static void loadBootProperties(URL directory, String environmentPropertiesLocation, Properties environmentProperties) throws IOException {
-
+    
+    public static void loadBootProperties(URL directory, String environmentPropertiesLocation,
+                                          Properties environmentProperties
+    ) throws IOException {
+        
         // priorities
         // low priority (default properties - environment props - user props - system properties) high priority
-
+        
         // properties from command line - environment properties
         URL bootPropertiesFile = new URL(directory, environmentPropertiesLocation);
         System.setProperty(ENVIRONMENT_PROPERTIES, environmentPropertiesLocation);
         environmentProperties.load(bootPropertiesFile.openStream());
-
+        
         // user properties
         String userBootPropertiesLocationsString = System.getProperty(USER_ENVIRONMENT_PROPERTIES);
         if (userBootPropertiesLocationsString == null) {
@@ -398,25 +365,25 @@ public final class JaggerLauncher {
             userBootPropertiesLocationsString = DEFAULT_USER_ENVIRONMENT_PROPERTIES_LOCATION;
         }
         String[] userBootPropertiesSingleLocations = userBootPropertiesLocationsString.split(",");
-
+        
         for (String location : userBootPropertiesSingleLocations) {
             URL userBootPropertiesFile = new URL(directory, location);
             Properties userBootProperties = new Properties();
             userBootProperties.load(userBootPropertiesFile.openStream());
-
+            
             for (String name : userBootProperties.stringPropertyNames()) {
                 // overwrite due to higher priority
                 environmentProperties.setProperty(name, userBootProperties.getProperty(name));
             }
         }
-
+        
         // default properties
         String defaultBootPropertiesLocation = System.getProperty(DEFAULT_ENVIRONMENT_PROPERTIES);
         if (defaultBootPropertiesLocation == null) {
             defaultBootPropertiesLocation = environmentProperties.getProperty(DEFAULT_ENVIRONMENT_PROPERTIES);
         }
-        if(defaultBootPropertiesLocation==null){
-            defaultBootPropertiesLocation=DEFAULT_ENVIRONMENT_PROPERTIES_LOCATION;
+        if (defaultBootPropertiesLocation == null) {
+            defaultBootPropertiesLocation = DEFAULT_ENVIRONMENT_PROPERTIES_LOCATION;
         }
         URL defaultBootPropertiesFile = new URL(directory, defaultBootPropertiesLocation);
         Properties defaultBootProperties = new Properties();
@@ -427,16 +394,17 @@ public final class JaggerLauncher {
                 environmentProperties.setProperty(name, defaultBootProperties.getProperty(name));
             }
         }
-
+        
         Properties properties = System.getProperties();
-        for (Enumeration<String> enumeration = (Enumeration<String>) properties.propertyNames(); enumeration.hasMoreElements(); ) {
+        for (Enumeration<String> enumeration = (Enumeration<String>) properties.propertyNames();
+             enumeration.hasMoreElements(); ) {
             String key = enumeration.nextElement();
             // overwrite due to higher priority
             environmentProperties.put(key, properties.get(key));
         }
-
+        
         System.setProperty(ENVIRONMENT_PROPERTIES, environmentPropertiesLocation);
-        System.setProperty(USER_ENVIRONMENT_PROPERTIES,userBootPropertiesLocationsString);
+        System.setProperty(USER_ENVIRONMENT_PROPERTIES, userBootPropertiesLocationsString);
         System.setProperty(DEFAULT_ENVIRONMENT_PROPERTIES, defaultBootPropertiesLocation);
     }
 }
--- chassis/util/src/main/java/com/griddynamics/jagger/util/JaggerXmlApplicationContext.java
+++ chassis/util/src/main/java/com/griddynamics/jagger/util/JaggerXmlApplicationContext.java
@@ -20,9 +20,9 @@
 
 package com.griddynamics.jagger.util;
 
+import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.web.context.support.XmlWebApplicationContext;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -31,7 +31,7 @@ import java.util.Properties;
 /**
  * User: dkotlyarov
  */
-public class JaggerXmlApplicationContext extends XmlWebApplicationContext {
+public class JaggerXmlApplicationContext extends AbstractXmlApplicationContext {
     private final URL directory;
     private final Properties environmentProperties;
 
