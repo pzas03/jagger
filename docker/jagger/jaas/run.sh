a8710fbb2176b749441aae7277d1600ee29c0ff4
Andrey Badaev <abadaev@griddynamics.com>
2016-11-26 20:41:36 +0300

    [JFG-1020] JaaS is not creating report (#715)
    * [JFG-1020] JaaS is not creating report
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh

--- docker/jagger/jaas/run.sh
+++ docker/jagger/jaas/run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Check if all important parameters exist:
 
 : "${JAAS_DB_DRIVER:?Need to set JAAS_DB_DRIVER non-empty}"   
@@ -8,24 +9,24 @@
 : "${JAAS_HIBERNATE_DIALECT:?Need to set JAAS_HIBERNATE_DIALECT non-empty}"   
 
 # Wait for database to get available
+echo "Waiting for mysql start - sleep 60"
+sleep 60
 
-MYSQL_WAIT=${MYSQL_WAIT}
+
+# Temporary disabled till JFG-1020
+#MYSQL_WAIT=${MYSQL_WAIT}
 
 #wait for mysql
-i=0
-while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
-  i=`expr $i + 1`
-  if [ $i -ge $MYSQL_WAIT ]; then
-    echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
-    exit 1
-  fi
-  echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}..."
-  sleep 1
-done
+#i=0
+#while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
+#  i=`expr $i + 1`
+#  if [ $i -ge $MYSQL_WAIT ]; then
+#    echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
+#    exit 1
+#  fi
+#  echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}..."
+#  sleep 1
+#done
 
-#continue with further steps
-
-#start the daemon
-sleep 1
 
 java -Djaas.hibernate.dialect=${JAAS_HIBERNATE_DIALECT} -Djaas.db.driver=${JAAS_DB_DRIVER} -Djaas.db.url=${JAAS_DB_URL} -Djaas.db.user=${JAAS_DB_USER} -Djaas.db.pass=${JAAS_DB_PASS} -Djagger.db.default.url=${JAGGER_DB_DEFAULT_URL} -Djagger.db.default.desc=${JAGGER_DB_DEFAULT_DESC} -Djagger.db.default.user=${JAGGER_DB_DEFAULT_USER} -Djagger.db.default.pass=${JAGGER_DB_DEFAULT_PASSWORD} -Djagger.db.default.jdbcDriver=${JAGGER_DB_DEFAULT_DRIVER} -Djagger.db.default.hibernateDialect=${JAGGER_DB_DEFAULT_HIBERNATE_DIALECT} -jar /com/griddynamics/jagger/jaas.jar  --server.port=${JAAS_HTTP_PORT}
--- docker/jagger/webclient/run.sh
+++ docker/jagger/webclient/run.sh
@@ -1,22 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Wait for database to get available
 
-MYSQL_WAIT=${MYSQL_WAIT}
+#wait for mysql
+echo "Waiting for mysql start - sleep 60"
+sleep 60
+
+# Temporary disabled till JFG-1020
+#MYSQL_WAIT=${MYSQL_WAIT}
 
 #wait for mysql
-i=0
-while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
-  i=`expr $i + 1`
-  if [ $i -ge $MYSQL_WAIT ]; then
-    echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
-    exit 1
-  fi
-  echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}..."
-  sleep 1
-done
+#i=0
+#while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
+#  i=`expr $i + 1`
+#  if [ $i -ge $MYSQL_WAIT ]; then
+#    echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
+#    exit 1
+#  fi
+#  echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}..."
+#  sleep 1
+#done
 
-#continue with further steps
 
-#start the daemon
-sleep 1
 java -jar jagger-webclient.jar -httpPort=${JWC_HTTP_PORT} -Djdbc.driver=${JWC_JDBC_DRIVER} -Djdbc.url=${JWC_JDBC_URL} -Djdbc.user=${JWC_JDBC_USER} -Djdbc.password=${JWC_JDBC_PASS} -Djdbc.hibernate.dialect=${JWC_HIBERNATE_DIALECT}
