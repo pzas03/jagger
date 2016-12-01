9b1478c91c9a1d59b6b3896c6ea278bca9bd2e7b
Mishin Nickolay <sanduku.default@gmail.com>
2016-12-01 23:07:51 +0300

    [JFG-1015]  change webclient dockerfile: container wait a connection in infinite cycle (#723)
    * [JFG-863_readme] split docker compose file, change readme
    
    split docker compose file for simplicity running in different modes.
    
    * [JFG-1015] change webclient dockerfile: container wait a connection in infinite cycle
    
    * [JFG-1015] delete MYSQL_WAIT from docker-compose for webclient
    
    * [JFG-1015] change jaas dockerfile: container wait a connection in infinite cycle

--- docker/jagger/jaas/run.sh
+++ docker/jagger/jaas/run.sh
@@ -8,25 +8,10 @@
 : "${JAAS_DB_PASS:?Need to set JAAS_DB_PASS non-empty}"   
 : "${JAAS_HIBERNATE_DIALECT:?Need to set JAAS_HIBERNATE_DIALECT non-empty}"   
 
-# Wait for database to get available
-echo "Waiting for mysql start - sleep 60"
-sleep 60
-
-
-# Temporary disabled till JFG-1020
-#MYSQL_WAIT=${MYSQL_WAIT}
-
-#wait for mysql
-#i=0
-#while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
-#  i=`expr $i + 1`
-#  if [ $i -ge $MYSQL_WAIT ]; then
-#    echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
-#    exit 1
-#  fi
-#  echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}..."
-#  sleep 1
-#done
-
+# wait for mysql
+while ! nc $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
+  echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}..."
+  sleep 1
+done
 
 java -Djaas.hibernate.dialect=${JAAS_HIBERNATE_DIALECT} -Djaas.db.driver=${JAAS_DB_DRIVER} -Djaas.db.url=${JAAS_DB_URL} -Djaas.db.user=${JAAS_DB_USER} -Djaas.db.pass=${JAAS_DB_PASS} -Djagger.db.default.url=${JAGGER_DB_DEFAULT_URL} -Djagger.db.default.desc=${JAGGER_DB_DEFAULT_DESC} -Djagger.db.default.user=${JAGGER_DB_DEFAULT_USER} -Djagger.db.default.pass=${JAGGER_DB_DEFAULT_PASSWORD} -Djagger.db.default.jdbcDriver=${JAGGER_DB_DEFAULT_DRIVER} -Djagger.db.default.hibernateDialect=${JAGGER_DB_DEFAULT_HIBERNATE_DIALECT} -jar /com/griddynamics/jagger/jaas.jar  --server.port=${JAAS_HTTP_PORT}
