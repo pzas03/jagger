ef7515190dfd3aa1ecd8a1d09757b10f9ba8244d
Anton Antonenko <kingtoxa@gmail.com>
2017-01-10 11:17:31 +0200

    JFG-970: remove Project API (#793)
    * JFG-911: remove more deprecated unused code
    
    * JFG-977: Remove redundant configurations
    
    * JFG-1069: make testProjectURL property of TestExecutionEntity nullable
    
    * JFG-970: remove Project API

--- jaas/src/main/java/com/griddynamics/jagger/jaas/storage/ProjectDao.java
+++
@@ -1,5 +0,0 @@
-package com.griddynamics.jagger.jaas.storage;
-
-import com.griddynamics.jagger.jaas.storage.model.ProjectEntity;
-
-public interface ProjectDao extends CrudDao<ProjectEntity, Long> {}
